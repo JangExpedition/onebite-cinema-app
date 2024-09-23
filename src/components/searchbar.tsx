89809da7eb1036a56b7566dbf9aa9263caf4f95a
jang_expedition <jangexpedition@gmail.com>
2024-09-23 20:58:41 +0900

    searchbar 구현

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,3 +1,39 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Searchbar() {
-  return <p>Searchbar Layout</p>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const q = searchParams.get("q");
+
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <input
+        type="text"
+        value={search}
+        placeholder="검색어를 입력해주세요."
+        onChange={onChangeHandler}
+        className="text-black"
+      />
+      <button type="submit">검색</button>
+    </form>
+  );
 }
