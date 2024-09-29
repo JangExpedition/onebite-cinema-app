e644e98f846643cc9700e6e453742cf13d1d1af8
jang_expedition <jangexpedition@gmail.com>
2024-09-29 23:49:41 +0900

    페이지 스트리밍 적용

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,14 +1,16 @@
 import MovieItem from "@/components/movie-item";
 import { MovieData } from "@/interface/movie";
+import { delay } from "@/util/delay";
 
 export default async function Page({
   searchParams,
 }: {
   searchParams: { q?: string };
 }) {
+  await delay(1500);
   const q = searchParams.q as string;
   const url = `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${q}`;
-  const response = await fetch(url, { cache: "no-cache" });
+  const response = await fetch(url, { cache: "force-cache" });
 
   if (!response.ok) {
     return <div>오류가 발생했습니다!</div>;
